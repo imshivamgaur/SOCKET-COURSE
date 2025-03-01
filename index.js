6ade1e5e2f4871adbaa89dfe28ec2342bbda88ee
iamshivamgaur <shivam24august@gmail.com>
2025-03-01 17:33:06 +0530

    basic-socket.io

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-//1. Packages
-
 import express from "express";
 import http from "http";
-
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import path from "path";
 import { Server } from "socket.io";
-
-//2. Instances
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-//3. Serving HTML File
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// Socket.io
+io.on("connection", (socket) => {//socket is like client
+  console.log("A new user has connected", socket.id);
+  socket.on("user-message", (message) => {
+    // console.log("New user message: ", message);
+    io.emit("message", message);
+  });
+});
 
-app.get("/", (req, res) => res.sendFile(join(__dirname, "index.html")));
+// middleware
+app.use(express.static(path.resolve("./public")));
 
-//4. Define a connection event handler
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
 
-//5. Start the server
 const PORT = 3000;
+server.listen(3000, () => console.log(`Server started at port ${PORT}`));
